8722752e06cc2d80205f571bbca539a3ef97c9f5
Akosah <emmanuel.s.akosah.19@gmail.com>
2023-02-08 16:27:19 -0500

    added payment options view

--- src/pages/invest.tsx
+++ src/pages/invest.tsx
@@ -1,13 +1,15 @@
 import FinancingOptionsView from '@components/organisms/FinancingOptionsView';
 import FixedFinancingView from '@components/organisms/FixedFinancingView';
+import PaymentOptionsView from '@components/organisms/PaymentOptionsView';
 import { Stack } from '@mui/material';
 import React from 'react';
 
 export const Invest: React.FC = () => {
   return (
     <Stack style={{ flex: 1, justifyContent: 'center' }}>
-      <FinancingOptionsView />
-      <FixedFinancingView />
+      {/* <FinancingOptionsView />
+      <FixedFinancingView /> */}
+      <PaymentOptionsView />
     </Stack>
   );
 };
